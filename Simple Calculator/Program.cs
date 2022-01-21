468026676e07fea6189436de002cc34f17b6b14a
Mateusz Skorupski <matsko2009@gmail.com>
2022-01-21 19:50:24 +0100

    .

--- Simple Calculator/Program.cs
+++ Simple Calculator/Program.cs
@@ -6,8 +6,31 @@ namespace Simple_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("This is a calculator.");
-            Console.WriteLine("2+2=4-1=3");
+            Console.WriteLine("This is a calculator, write 1 number.");
+            var nmbr1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("write 2th number");
+            var nmbr2 = int.Parse(Console.ReadLine());
+            var operation = Console.ReadLine();
+            var wyn = 0;
+            switch (operation)
+            {
+
+                case "+":
+                    wyn = nmbr1 + nmbr2;
+                    break;
+                case "-":
+                    wyn = nmbr1 - nmbr2;
+                    break;
+                case "*":
+                    wyn = nmbr1 * nmbr2;
+                    break;
+                case "/":
+                    wyn = nmbr1 / nmbr2;
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine("wynik dzialania to", wyn);
         }
     }
 }
